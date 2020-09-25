bd322df68dc44e1528f6a9ea3ca886411524c005
gustavox13 <gurodrigues1300@gmail.com>
2020-09-25 19:39:07 -0300

    add animations and assets

---
+++ Assets/Scripts/MouseHover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+    
+    private Animator hoveranim;
+
+
+    private void Start()
+    {
+        hoveranim = gameObject.GetComponent<Animator>();
+    }
+
+
+    private void OnMouseEnter()
+    {
+        hoveranim.SetBool("mousehover", true);
+    }
+
+    private void OnMouseExit()
+    {
+        hoveranim.SetBool("mousehover", false);
+    }
+}
